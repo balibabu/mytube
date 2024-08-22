23d480ff1ae9a144da5ae73848c0a24827231f3e
balibabu <balibabuchauhan1@gmail.com>
2024-08-22 10:24:07 +0545

    fixed url name

--- src/components/extractor.js
+++ src/components/extractor.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function getLinks(setItems = () => { }) {
     const vlist = [];
-    const response = await axios.get('https://raw.githubusercontent.com/balibabu/mytube/main/urls.txt');
+    const response = await axios.get('https://raw.githubusercontent.com/balibabu/mytube/main/urls');
     const texts = response.data.trim();
     const items = texts.split('\n\n');
 
