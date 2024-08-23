1af27afa49414cffd9b51fe775c9fe499de24d48
balibabu <balibabuchauhan1@gmail.com>
2024-08-23 13:11:30 +0545

    added share button

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ export default function App() {
     return (
         <div className='w-full sm:w-1/3 mx-auto border-x-2 border-gray-500'>
             <div className='bg-gray-500 text-3xl font-mono text-center font-bold py-1 tracking-widest text-green-950'>VIDEOS</div>
-            {items.map((item, index) => <VidCard key={index} {...{ item }} />)}
+            {items.map((item, index) => <VidCard key={index} {...{ item, vid: index }} />)}
         </div>
     )
 }
--- src/components/VidCard.jsx
+++ src/components/VidCard.jsx
@@ -1,8 +1,11 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import Share from './Share';
 
-export default function VidCard({ item }) {
+export default function VidCard({ item, vid }) {
     const [quality, setQuality] = useState(0);
     const videoRef = useRef(null);
+    const cardRef = useRef(null);
+    useEffect(() => { if (window.location.hash === `#${vid}`) cardRef.current.scrollIntoView({ behavior: 'smooth' }) }, []);
 
     function changeQuality(e) {
         const index = e.target.value;
@@ -10,20 +13,25 @@ export default function VidCard({ item }) {
         const currentTime = videoRef.current.currentTime;
         videoRef.current.src = item.video[index];
         videoRef.current.currentTime = currentTime;
-        videoRef.current.addEventListener('loadeddata', () => {
-            videoRef.current.play();
-        }, { once: true });
+        videoRef.current.addEventListener('loadeddata', () => videoRef.current.play(), { once: true });
     }
 
+    function copyToClip() {
+        navigator.clipboard.writeText(window.location.origin + `/mytube#${vid}`);
+        alert('url copied to clipboard');
+    }
 
     return (
         <div className='bg-white border-2 border-black m-1 rounded overflow-clip'>
-            <div className='bg-green-300 px-2 font-bold'>{item.title}</div>
+            <div className='bg-green-300 px-2 py-1 font-bold flex justify-between' ref={cardRef} id={vid}>
+                <div>{item.title}</div>
+                <div className='w-5 cursor-pointer hover:text-blue-600' onClick={copyToClip}><Share {...{ strokeWidth: 2 }} /></div>
+            </div>
             <div className='overflow-hidden'>
                 <div className='relative'>
-                    <div className='absolute z-10 right-0 opacity-25 hover:opacity-100 cursor-pointer font-bold'>quality
+                    <div className='absolute z-10 right-0 opacity-50 hover:opacity-100 cursor-pointer font-bold'>
                         <select value={quality} onChange={changeQuality}>
-                            {item.video.map((link, index) => <option key={index} value={index}>A{index + 1}</option>)}
+                            {item.video.map((link, index) => <option key={index} value={index}>{index < 2 ? ['low', 'high'][index] : `Level-${index + 1}`}</option>)}
                         </select>
                     </div>
                     <video preload='none' controls className='w-full' ref={videoRef} poster={item.thumbnail} src={item.video[quality]} />
